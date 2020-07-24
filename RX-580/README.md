0bbc8818049315bf56ca83c2e52dc4a41d384245
Rodrigo Leutz <rodrigo@uware.com.br>
2020-07-24 14:51:40 -0300

    Adicionado overclock utilizado no teste

---
+++ RX-580/README.md
@@ -0,0 +1,5 @@
+Overclock utilizado na Radeon RX 580 8GB
+
+Faça e use por sua conta e risco, não me responsabilizo por danos causados a placa.
+
+
---
+++ RX-580/rx580extreme.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "manual" > /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/power_dpm_force_performance_level
+echo 185000000 > /sys/class/hwmon/hwmon3/power1_cap 
+echo "s 0 300 750" > /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/pp_od_clk_voltage 
+echo "s 1 600 769" > /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/pp_od_clk_voltage 
+echo "s 2 900 862" > /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/pp_od_clk_voltage 
+echo "s 3 1145 1068" > /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/pp_od_clk_voltage 
+echo "s 4 1215 1110" > /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/pp_od_clk_voltage 
+echo "s 5 1255 1130" > /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/pp_od_clk_voltage 
+echo "s 6 1366 1150" > /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/pp_od_clk_voltage 
+echo "s 7 1490 1195" > /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/pp_od_clk_voltage 
+echo "m 0 300 750" > /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/pp_od_clk_voltage 
+echo "m 1 1000 800" > /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/pp_od_clk_voltage 
+echo "m 2 2250 1000" > /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/pp_od_clk_voltage 
+echo "c" > /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/pp_od_clk_voltage
+
